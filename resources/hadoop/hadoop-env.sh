201d8d8647dca53c74a20111c192e74e878e7eae
loganfreeman <shanhongcheng@hotmail.com>
2016-03-09 23:48:12 -0700

    all

--- resources/hadoop/hadoop-env.sh
+++ resources/hadoop/hadoop-env.sh
@@ -1,5 +1,5 @@
 # Copyright 2011 The Apache Software Foundation
-# 
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -70,7 +70,7 @@ export HADOOP_SECURE_DN_USER=${HADOOP_SECURE_DN_USER}
 export HADOOP_SECURE_DN_LOG_DIR=${HADOOP_LOG_DIR}/${HADOOP_HDFS_USER}
 
 # The directory where pid files are stored. /tmp by default.
-# NOTE: this should be set to a directory that can only be written to by 
+# NOTE: this should be set to a directory that can only be written to by
 #       the user that will run the hadoop daemons.  Otherwise there is the
 #       potential for a symlink attack.
 export HADOOP_PID_DIR=${HADOOP_PID_DIR}
